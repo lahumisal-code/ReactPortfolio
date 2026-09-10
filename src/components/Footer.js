59ed4c07c490d5c17bac3a9929bce79f8502dd5a
Lahu Misal <lahumisal402@gmail.com>
2026-09-10 22:28:38 +1400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,7 +24,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/lahumisal"
+                href="https://github.com/lahumisal-code"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
